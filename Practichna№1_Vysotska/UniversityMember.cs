94690f1b436cfd948c289b654c94c70b31ec4a81
DarkkkSonick <vysotskasophia14@gmail.com>
2026-05-14 21:42:18 +0300

    Змінено внутрішній список групи на List<UniversityMember>

--- Practichna№1_Vysotska/UniversityMember.cs
+++ Practichna№1_Vysotska/UniversityMember.cs
@@ -1,14 +1,7 @@
-﻿using System;
-
-public abstract class UniversityMember : Person
+﻿public abstract class UniversityMember : Person
 {
     protected UniversityMember(string fullName, DateTime dob, string email)
         : base(fullName, dob, email) { }
 
     public abstract decimal CalculateScholarship();
-
-    public virtual void Enroll()
-    {
-        Console.WriteLine($"{FullName} зараховано до списків.");
-    }
 }
